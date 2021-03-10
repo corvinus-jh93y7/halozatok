12bcaa8d220f1f0d3379b63abae1448d4c348105
corvinus-jh93y7 <matyas.novothny@stud.uni-corvinus.hu>
2021-03-10 15:07:01 +0100

    reg

---
+++ regularis/regularis/Form1.Designer.cs
@@ -0,0 +1,94 @@
+﻿
+namespace regularis
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxText = new System.Windows.Forms.TextBox();
+            this.textBoxRegEx = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(27, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(107, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ellenőrizendő szöveg";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(27, 183);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(95, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Reguláris kifejezés";
+            // 
+            // textBoxText
+            // 
+            this.textBoxText.Location = new System.Drawing.Point(30, 74);
+            this.textBoxText.Name = "textBoxText";
+            this.textBoxText.Size = new System.Drawing.Size(262, 20);
+            this.textBoxText.TabIndex = 2;
+            // 
+            // textBoxRegEx
+            // 
+            this.textBoxRegEx.Location = new System.Drawing.Point(30, 220);
+            this.textBoxRegEx.Name = "textBoxRegEx";
+            this.textBoxRegEx.Size = new System.Drawing.Size(262, 20);
+            this.textBoxRegEx.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBoxRegEx);
+            this.Controls.Add(this.textBoxText);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxText;
+        private System.Windows.Forms.TextBox textBoxRegEx;
+    }
+}
+
---
+++ regularis/regularis/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace regularis
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBoxText_TextChanged(object sender, EventArgs e)
+        {
+            DoCheck();
+        }
+
+        private void textBoxRegEx_TextChanged(object sender, EventArgs e)
+        {
+            DoCheck();
+        }
+
+        void DoCheck()
+        {
+            try
+            {
+                Regex regex = new Regex(textBoxRegEx.Text);
+                if (regex.IsMatch(textBoxText.Text))
+                {
+                    textBoxText.BackColor = Color.LightGreen;
+                }
+                else
+                {
+                    textBoxText.BackColor = Color.LightCoral;
+                }
+            }
+            catch (Exception)
+            {
+                textBoxRegEx.BackColor = Color.LightCoral;
+                return;
+            }
+            textBoxRegEx.BackColor = Color.LightGreen;
+        }
+    }
+}
