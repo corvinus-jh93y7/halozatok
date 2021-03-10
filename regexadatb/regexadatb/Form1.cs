8e1cd683eeecd676d5e5ad0f43c2491a04d0e9ee
corvinus-jh93y7 <matyas.novothny@stud.uni-corvinus.hu>
2021-03-10 18:24:09 +0100

    joe

--- regexadatb/regexadatb/Form1.cs
+++ regexadatb/regexadatb/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.Entity;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,9 +13,23 @@ namespace regexadatb
 {
     public partial class Form1 : Form
     {
+
+        Database1Entities context = new Database1Entities();
         public Form1()
         {
             InitializeComponent();
+            context.People.Load();
+            personBindingSource.DataSource = context.People.Local;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Person p = new Person();
+            p.Name = "Joe";
+
+            personBindingSource.Add(p);
+            context.SaveChanges();
+            
         }
     }
 }
