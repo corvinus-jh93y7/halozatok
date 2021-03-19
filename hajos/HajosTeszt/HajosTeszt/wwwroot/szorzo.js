a9121c39c25ebd1d1a56b57e9f4a505ec4c702a1
corvinus-jh93y7 <matyas.novothny@stud.uni-corvinus.hu>
2021-03-19 13:30:28 +0100

    szorzotabla_felepites

---
+++ hajos/HajosTeszt/HajosTeszt/wwwroot/szorzo.js
@@ -0,0 +1,22 @@
+﻿window.onload = function () {
+    console.log("start")
+
+    let hova = document.getElementById("ide");
+    for (var s = 0; s < 10; s++) {
+        let sor = document.createElement("div");
+        sor.classList.add("sor")
+        hova.appendChild(sor);
+
+
+        for (var o = 0; o < 10; o++) {
+            let szám = document.createElement("div");
+            szám.classList.add("elem")
+            sor.appendChild(szám);
+            szám.innerText = (s + 1) * (o + 1);
+
+
+
+        } 
+
+    }
+}
