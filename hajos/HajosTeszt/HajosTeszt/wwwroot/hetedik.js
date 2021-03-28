d525f586228f101f00889c1459399040e8186fa0
corvinus-jh93y7 <matyas.novothny@stud.uni-corvinus.hu>
2021-03-28 13:09:22 +0200

    alapok_json

---
+++ hajos/HajosTeszt/HajosTeszt/wwwroot/hetedik.js
@@ -0,0 +1,15 @@
+﻿var kérdések;
+function letöltés()
+{
+    fetch('/questions.json')
+        .then(response => response.json())
+        .then(data => letöltésBefejeződött(data)
+        );    
+}
+
+function letöltésBefejeződött(d)
+{
+    console.log("Sikeres letöltés")
+    console.log(d)
+    kérdések = d;
+}
